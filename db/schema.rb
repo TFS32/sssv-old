4291fdb0815028e54497d30f238469beacb43ba4
Henrique Luna <jhmluna@gmail.com>
2021-11-05 20:58:00 -0300

    Resolve pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_11_05_180614) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_11_05_180614) do
     t.string "cpf"
     t.string "phone_number"
     t.string "address"
-    t.string "role"
+    t.string "role", default: "citizen"
     t.string "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
