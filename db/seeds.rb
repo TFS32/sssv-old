67623a404f740c6f04305c88bf592bfd127c8eb2
tiago saraiva <tiagofsaraiva@hotmail.com>
2021-11-06 18:29:16 -0300

    commented seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-puts "Starting to seed"
-puts "..."
-user1 = User.create!(full_name: 'John Snow', email: 'night_watcher@gmail.com', password: '123456')
-user2 = User.create!(full_name: 'Daenerys Targaryen', email: 'stormborn@gmail.com', password: '123456')
-user3 = User.create!(full_name: 'Tyrion Lannister', email: 'dwarfs@gmail.com', password: '123456', role: 'tech')
-user4 = User.create!(full_name: 'Cidadão Instigado', email: 'citizen@gmail.com', password: '123456')
-user5 = User.create!(full_name: 'Servidor Atarefado', email: 'worker@gmail.com', password: '123456', role: 'tech')
-user6 = User.create!(full_name: 'Gerente Irritado', email: 'manager@gmail.com', password: '123456', role: 'manager')
-Request.create!(instrument: 'gas', service: 'verification', status: 'aberta', citizen: user1)
-Request.create!(instrument: 'luz', service: 'expertise', status: 'aberta', citizen: user2, tech: user3)
-puts "Seed completed"
+# puts "Starting to seed"
+# puts "..."
+# user1 = User.create!(full_name: 'John Snow', email: 'night_watcher@gmail.com', password: '123456')
+# user2 = User.create!(full_name: 'Daenerys Targaryen', email: 'stormborn@gmail.com', password: '123456')
+# user3 = User.create!(full_name: 'Tyrion Lannister', email: 'dwarfs@gmail.com', password: '123456', role: 'tech')
+# user4 = User.create!(full_name: 'Cidadão Instigado', email: 'citizen@gmail.com', password: '123456')
+# user5 = User.create!(full_name: 'Servidor Atarefado', email: 'worker@gmail.com', password: '123456', role: 'tech')
+# user6 = User.create!(full_name: 'Gerente Irritado', email: 'manager@gmail.com', password: '123456', role: 'manager')
+# Request.create!(instrument: 'gas', service: 'verification', status: 'aberta', citizen: user1)
+# Request.create!(instrument: 'luz', service: 'expertise', status: 'aberta', citizen: user2, tech: user3)
+# puts "Seed completed"
 
 # Enter this into the Rails console to instantiate users and requests:
 # user1 = User.find(1)
