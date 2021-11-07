b780a53d7bb4cf8630ba1d52df209017c44884f7
Augusto Mendonca <augusto.sousa@economia.gov.br>
2021-11-07 17:42:18 -0300

    inclui campo location e faz tratamento

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_013747) do
+ActiveRecord::Schema.define(version: 2021_11_07_195545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_013747) do
     t.string "address"
     t.string "role", default: "citizen"
     t.string "full_name"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
