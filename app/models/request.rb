e6d7501b15cf16b287dc2b0dc93127a127e488e2
Henrique Luna <jhmluna@gmail.com>
2021-11-07 18:01:55 -0300

    Add dropdown in form

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,8 @@
 class Request < ApplicationRecord
+  INSTRUMENTS = %w[Medidor\ de\ Energia\ Elétrica Medidor\ de\ Água Medidor\ de\ Gás]
   SERVICES = %w[Verification Expertise].freeze
+  STATES = %w[Acre Alagoas Amapá Amazonas Bahia Ceará Espírito\ Santo Goiás Maranhão Mato\ Grosso Mato\ Grosso\ do\ Sul
+              Minas\ Gerais Pará Paraíba Paraná Pernambuco Piauí Rio\ de\ Janeiro Rio\ Grande\ do\ Norte].freeze
   belongs_to :citizen, class_name: 'User'
   belongs_to :tech, optional: true, class_name: 'User'
 end
