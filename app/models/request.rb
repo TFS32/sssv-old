b42551a0f8ba70f321d967106c0c1afd7e7228fd
Augusto Mendonca <augusto.sousa@economia.gov.br>
2021-11-07 17:54:03 -0300

    inclui campo location e faz tratamento

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :citizen, class_name: 'User'
   belongs_to :tech, optional: true, class_name: 'User'
+  validates :instrument, :service, :location, :description, presence:true
 end
