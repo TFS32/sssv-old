c5d9ce5beca6eecd80dab1a2b3c3ed51bdb0bf23
Henrique Luna <jhmluna@gmail.com>
2021-11-07 10:28:10 -0300

    Created views based on role

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :requests, except: :destroy
-  # UPDATE
+  # Update requests
   patch 'requests/get/:id', to: 'requests#get', as: :request_get
 end
