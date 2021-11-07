d827d585af89d4519d0d5422681e50a4533eb800
Henrique Luna <jhmluna@gmail.com>
2021-11-07 11:58:32 -0300

    Service constant in model and fix identation

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
+  SERVICES = %w[Verification Expertise].freeze
   belongs_to :citizen, class_name: 'User'
   belongs_to :tech, optional: true, class_name: 'User'
 end
